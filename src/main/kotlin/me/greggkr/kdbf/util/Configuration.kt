db89d92d36ea56b7c1c584ab23d9ec7555294c70
Grzegorz <greggkrzykowski@gmail.com>
2019-11-18 14:38:42 -0600

    ability to hide any config variables you want.

---
+++ src/main/kotlin/me/greggkr/kdbf/util/Configuration.kt
@@ -0,0 +1,5 @@
+package me.greggkr.kdbf.util
+
+import java.io.File
+
+class Configuration(val file: File, val toHide: List<String> = listOf("token"))
--- src/test/kotlin/me/greggkr/kdbf/TestBot.kt
+++ src/test/kotlin/me/greggkr/kdbf/TestBot.kt
@@ -1,10 +1,14 @@
 package me.greggkr.kdbf
 
 import me.greggkr.kdbf.command.discoverCommands
+import me.greggkr.kdbf.util.Configuration
 import java.io.File
 
 fun main() {
     discoverCommands("me.greggkr.kdbf")
-    val file = File("src/test/resources/config.json")
-    startBot(file)
+    val configuration = Configuration(
+        File("src/test/resources/config.json"),
+        listOf("token", "test_hidden_value")
+    )
+    startBot(configuration)
 }
